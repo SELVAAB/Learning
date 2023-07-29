56bb2dab87a96b5e2341755a74f9a2928ff3c62a
SELVAAB <140791548+SELVAAB@users.noreply.github.com>
2023-07-29 13:25:12 +0530

    initial commit

---
+++ Windowhandling .java
@@ -0,0 +1,9 @@
+public class Windowhandling {
+
+	
+	public static void main(String[] args) throws InterruptedException   {
+		
+		System.out.println("first");
+		System.out.println("second");
+}
+}
